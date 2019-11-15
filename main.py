0a73f5b2ba01afa49d2518dc607cc358e408c304
Nick <ngonzo95@gmail.com>
2019-11-15 10:40:17 -0600

    Added messages to the user when they give improper input

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 from BarrenLandAnalysis.model.field import Field
 from BarrenLandAnalysis.service.parseInput import parseBarrenFieldInput
+from BarrenLandAnalysis.exception.fieldParsingException import (
+    FieldParsingException)
 
 
 def main():
+    """
+    Takes in user specified barren land and outputs the area of the reamining
+    Land
+    """
+    # Initalize a new feild with new barren spots
     field = Field(400, 600)
-    strInput = input()
 
-    barrenFields = parseBarrenFieldInput(strInput)
+    try:
+        # Convert the input into a list of rectangles
+        strInput = input()
+        barrenFields = parseBarrenFieldInput(strInput)
+    except FieldParsingException as e:
+        # If there is a parsing error alert the user and exit
+        print(str(e))
+        return
 
+    # Subract all of the barren feilds from the field
     for barrenField in barrenFields:
         field.removeBarrenArea(barrenField.createContinuosBox())
 
+    # Calculate the area from the remaining feild
     areaList = field.area()
 
+    # Print out the area list in the desired format
     outputString = ""
-
     for area in areaList:
         outputString += str(int(area)) + " "
 
